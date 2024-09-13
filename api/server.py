d62136b94b32c0576fa2d77066df1c012bc22d93
Yash <yash.magare1001@gmail.com>
2024-09-13 16:51:14 +0530

    adding server changes

--- api/server.py
+++ api/server.py
@@ -2,14 +2,17 @@ from flask import Flask, jsonify, request
 import razorpay
 from dotenv import load_dotenv
 import os
+
+# Uncomment the following line if using nemo_toolkit
 # import nemo.collections.asr as nemo_asr
 
+# Uncomment the following lines if you need speaker recognition
 # speaker_model = nemo_asr.models.EncDecSpeakerLabelModel.from_pretrained("nvidia/speakerverification_en_titanet_large")
-
 
 load_dotenv()
 app = Flask(__name__)
 
+# Retrieve Razorpay credentials from environment variables
 rzp_key = os.getenv('RAZORPAY_KEY')
 secret_key = os.getenv('RAZORPAY_SECRET_KEY')
 
@@ -22,6 +25,7 @@ def index():
 def members():
     return jsonify({"name": "John"})
 
+# Uncomment the following route handlers if using nemo_toolkit
 # @app.route('/audio/embbed')
 # def audio():
 #     emb = speaker_model.get_embedding("./output_audio.wav")
@@ -48,5 +52,8 @@ def deposit(n):
     except Exception as e:
         return jsonify({"error": str(e)}), 500
 
+# The following line should be removed for Vercel deployment
+# app.run()
 
-app.run()
+if __name__ == "__main__":
+    app.run(debug=True)
